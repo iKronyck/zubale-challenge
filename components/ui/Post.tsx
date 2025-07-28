c286aa8a8b2efb5e749c9da43429220b8efb5b66
Israel Hernández <israel.devalfaro@gmail.com>
2025-07-27 22:42:25 -0600

    feat(components): update Post component to accept dynamic post data as props for header, image, actions, and footer

--- components/ui/Post.tsx
+++ components/ui/Post.tsx
@@ -1,16 +1,30 @@
+import { Post as PostType } from "@/types";
 import { View } from "tamagui";
 import { PostActions } from "./PostActions";
 import { PostFooter } from "./PostFooter";
 import { PostHeader } from "./PostHeader";
 import { PostImage } from "./PostImage";
 
-export function Post() {
+export function Post({ post }: { post: PostType }) {
   return (
     <View w="100%" bg="$background" py={"$2"}>
-      <PostHeader />
-      <PostImage />
-      <PostActions />
-      <PostFooter />
+      <PostHeader
+        avatar={post.avatar}
+        name={post.name}
+        location={post.location}
+      />
+      <PostImage image={post.image} />
+      <PostActions
+        likes={post.likes}
+        comments={post.comments}
+        saved={post.saved}
+        liked={post.liked}
+      />
+      <PostFooter
+        description={post.description}
+        createdAt={post.createdAt}
+        name={post.name}
+      />
     </View>
   );
 }
