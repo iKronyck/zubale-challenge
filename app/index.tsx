5d1c1a59ac684029707f47dc38fc6b927a3f3220
Israel Hernández <israel.devalfaro@gmail.com>
2025-07-27 23:48:03 -0600

    refactor(HomeScreen): replace MainLayout with View and integrate InstagramHeader for enhanced UI

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,14 @@
-import { MainLayout } from "@/components/layouts/MainLayout";
+import { InstagramHeader } from "@/components/ui/InstagramHeader";
 import { PostList } from "@/components/ui/PostList";
 import { StoriesList } from "@/components/ui/StoriesList";
+import { View } from "tamagui";
 
-export default function Home() {
+export default function HomeScreen() {
   return (
-    <MainLayout>
+    <View flex={1} backgroundColor="$background" paddingTop="$4">
+      <InstagramHeader />
       <StoriesList />
       <PostList />
-    </MainLayout>
+    </View>
   );
 }
