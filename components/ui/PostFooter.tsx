e4af556c682daed1008684db21587bcad99fda62
Israel Hernández <israel.devalfaro@gmail.com>
2025-07-27 20:38:52 -0600

    feat(components): add PostFooter component to display likes and comments for posts

---
+++ components/ui/PostFooter.tsx
@@ -0,0 +1,28 @@
+import { Image, Text, View } from "tamagui";
+
+export function PostFooter() {
+  return (
+    <View flexDirection="column" gap={"$2"}>
+      <View flexDirection="row" gap={"$2"}>
+        <Image
+          source={{
+            uri: "https://picsum.photos/200/300",
+          }}
+          w={20}
+          h={20}
+          borderRadius="$10"
+        />
+        <Text color="$textPrimary" fontSize={16}>
+          Liked by <Text fontWeight="700">Israel</Text> and others
+        </Text>
+      </View>
+      <Text color="$textPrimary" fontSize={16}>
+        <Text fontWeight="700">{`Israel `}</Text>
+        <Text pl="$2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+          quos.
+        </Text>
+      </Text>
+    </View>
+  );
+}
