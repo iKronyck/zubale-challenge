2ac7be4f7c1bf407fc3eb308c2d8e83cd8596a50
Israel Hernández <israel.devalfaro@gmail.com>
2025-07-27 23:48:10 -0600

    feat(layout): wrap Tabs in SafeAreaView for improved layout and add tab screens with icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,7 @@ import { useColorScheme } from "@/hooks/useColorScheme";
 import tamaguiConfig from "@/tamagui.config";
 import { Foundation, Ionicons, Octicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { Image, TamaguiProvider } from "tamagui";
 
 export default function RootLayout() {
@@ -24,70 +25,74 @@ export default function RootLayout() {
   return (
     <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme}>
       <QueryClientProvider client={queryClient}>
-        <Tabs
-          screenOptions={{
-            tabBarStyle: {
-              backgroundColor: Colors[colorScheme].background,
-              borderTopWidth: 0,
-              height: 60,
-              paddingBottom: 8,
-              paddingTop: 8,
-            },
-            tabBarActiveTintColor: Colors[colorScheme].textPrimary,
-            tabBarInactiveTintColor: Colors[colorScheme].textSecondary,
-            tabBarShowLabel: false,
-            headerShown: false,
-          }}
+        <SafeAreaView
+          style={{ flex: 1, backgroundColor: Colors[colorScheme].background }}
         >
-          <Tabs.Screen
-            name="index"
-            options={{
-              tabBarIcon: ({ color }) => (
-                <Foundation name={"home"} size={28} color={color} />
-              ),
+          <Tabs
+            screenOptions={{
+              tabBarStyle: {
+                backgroundColor: Colors[colorScheme].background,
+                borderTopWidth: 0,
+                height: 60,
+                paddingBottom: 8,
+                paddingTop: 8,
+              },
+              tabBarActiveTintColor: Colors[colorScheme].textPrimary,
+              tabBarInactiveTintColor: Colors[colorScheme].textSecondary,
+              tabBarShowLabel: false,
+              headerShown: false,
             }}
-          />
-          <Tabs.Screen
-            name="search"
-            options={{
-              tabBarIcon: ({ color }) => (
-                <Ionicons name={"search"} size={28} color={color} />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name="create"
-            options={{
-              tabBarIcon: ({ color }) => (
-                <Octicons name={"diff-added"} size={28} color={color} />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name="reels"
-            options={{
-              tabBarIcon: ({ color }) => (
-                <Ionicons name={"heart-outline"} size={28} color={color} />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name="profile"
-            options={{
-              tabBarIcon: () => (
-                <Image
-                  source={{
-                    uri: "https://avatars.githubusercontent.com/u/12678606?v=4",
-                  }}
-                  width={28}
-                  height={28}
-                  borderRadius={14}
-                />
-              ),
-            }}
-          />
-        </Tabs>
-        <StatusBar style="auto" />
+          >
+            <Tabs.Screen
+              name="index"
+              options={{
+                tabBarIcon: ({ color }) => (
+                  <Foundation name={"home"} size={28} color={color} />
+                ),
+              }}
+            />
+            <Tabs.Screen
+              name="search"
+              options={{
+                tabBarIcon: ({ color }) => (
+                  <Ionicons name={"search"} size={28} color={color} />
+                ),
+              }}
+            />
+            <Tabs.Screen
+              name="create"
+              options={{
+                tabBarIcon: ({ color }) => (
+                  <Octicons name={"diff-added"} size={28} color={color} />
+                ),
+              }}
+            />
+            <Tabs.Screen
+              name="reels"
+              options={{
+                tabBarIcon: ({ color }) => (
+                  <Ionicons name={"heart-outline"} size={28} color={color} />
+                ),
+              }}
+            />
+            <Tabs.Screen
+              name="profile"
+              options={{
+                tabBarIcon: () => (
+                  <Image
+                    source={{
+                      uri: "https://avatars.githubusercontent.com/u/12678606?v=4",
+                    }}
+                    width={28}
+                    height={28}
+                    borderRadius={14}
+                  />
+                ),
+              }}
+            />
+          </Tabs>
+          <StatusBar style="auto" />
+        </SafeAreaView>
       </QueryClientProvider>
     </TamaguiProvider>
   );
