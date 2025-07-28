c8fd3d22293fb5b9772051ed52b6252428c5227f
Israel Hernández <israel.devalfaro@gmail.com>
2025-07-27 23:43:51 -0600

    feat(layout): replace Stack navigation with Tabs for improved navigation structure and add tab icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,10 +3,12 @@ import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 
+import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import tamaguiConfig from "@/tamagui.config";
-import { Stack } from "expo-router";
-import { TamaguiProvider } from "tamagui";
+import { Foundation, Ionicons, Octicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import { Image, TamaguiProvider } from "tamagui";
 
 export default function RootLayout() {
   const queryClient = new QueryClient();
@@ -22,9 +24,69 @@ export default function RootLayout() {
   return (
     <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme}>
       <QueryClientProvider client={queryClient}>
-        <Stack>
-          <Stack.Screen name="index" options={{ headerShown: true }} />
-        </Stack>
+        <Tabs
+          screenOptions={{
+            tabBarStyle: {
+              backgroundColor: Colors[colorScheme].background,
+              borderTopWidth: 0,
+              height: 60,
+              paddingBottom: 8,
+              paddingTop: 8,
+            },
+            tabBarActiveTintColor: Colors[colorScheme].textPrimary,
+            tabBarInactiveTintColor: Colors[colorScheme].textSecondary,
+            tabBarShowLabel: false,
+            headerShown: false,
+          }}
+        >
+          <Tabs.Screen
+            name="index"
+            options={{
+              tabBarIcon: ({ color }) => (
+                <Foundation name={"home"} size={28} color={color} />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="search"
+            options={{
+              tabBarIcon: ({ color }) => (
+                <Ionicons name={"search"} size={28} color={color} />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="create"
+            options={{
+              tabBarIcon: ({ color }) => (
+                <Octicons name={"diff-added"} size={28} color={color} />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="reels"
+            options={{
+              tabBarIcon: ({ color }) => (
+                <Ionicons name={"heart-outline"} size={28} color={color} />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="profile"
+            options={{
+              tabBarIcon: () => (
+                <Image
+                  source={{
+                    uri: "https://avatars.githubusercontent.com/u/12678606?v=4",
+                  }}
+                  width={28}
+                  height={28}
+                  borderRadius={14}
+                />
+              ),
+            }}
+          />
+        </Tabs>
         <StatusBar style="auto" />
       </QueryClientProvider>
     </TamaguiProvider>
